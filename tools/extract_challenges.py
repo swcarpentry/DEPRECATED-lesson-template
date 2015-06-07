140df2818dca8b137665ba5656be3235546062a1
Greg Wilson <gvwilson@third-bit.com>
2015-06-07 18:04:36 -0400

    Adding simple tool to extract challenges from Markdown files

---
+++ tools/extract_challenges.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+'''Extract challenges from lessons.'''
+
+import sys
+import re
+
+
+USAGE = 'usage: extract_challenges [-o output_name] [filename...]'
+
+title_pat = re.compile(r'^>\s+.+\{.challenge\}')
+block_pat = re.compile(r'^>')
+
+def main(argv):
+    '''Main driver.'''
+
+    prog_name = argv[0]
+    filenames = argv[1:]
+    output_name = None
+    writer = sys.stdout
+
+    if filenames and (filenames[0] == '-o'):
+        if len(filenames) < 2:
+            fail(USAGE)
+        output_name = filenames[1]
+        filenames = filenames[2:]
+
+    if output_name:
+        writer = open(output_name, 'w')
+
+    if filenames:
+        for f in filenames:
+            with open(f, 'r') as reader:
+                extract(reader, sys.stdout)
+    else:
+        extract(sys.stdin, sys.stdout)
+
+    if writer != sys.stdout:
+        writer.close()
+
+
+def extract(reader, writer):
+    '''Extract challenges from reader, sending to writer.'''
+
+    echo = False
+    for line in reader:
+        if title_pat.search(line):
+            echo = True
+        if echo and (not block_pat.search(line)):
+            print >> writer
+            echo = False
+        if echo:
+            print >> writer, line.rstrip()
+    print >> writer
+
+
+if __name__ == '__main__':
+    main(sys.argv)
